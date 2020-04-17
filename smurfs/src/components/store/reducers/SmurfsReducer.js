1e984853fe83c14da9b3cea00e06dff09e8e986b
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-17 09:42:50 -0600

    created SmurfsReducer with Fetching initialState

---
+++ smurfs/src/components/store/reducers/SmurfsReducer.js
@@ -0,0 +1,45 @@
+const initialState = {
+    character:[
+        {
+            name: '',
+            age: null,
+            height: '',
+            id:Date.now()
+        }
+    ],
+    isFetching:false,
+    error:''
+}
+
+export const SmurfsReducer = (state = initialState, action) =>{
+    switch(action.type){
+        case 'FETCH_SMURFS_START':
+            return{
+                ...state,
+                isFetching:true,
+                error:''
+            }
+
+        case 'FETCH_SMURFS_SUCCESS':
+            return{
+                ...state,
+                character:[{
+                    name:action.payload.name,
+                    age:action.payload.age,
+                    height:action.payload.height,
+                    id:action.payload.id
+                }],
+                isFetching:false
+            }
+
+        case 'FETCH_SMURFS_END':
+            return{
+                ...state,
+                isFetching:false,
+                error:action.payload
+            }
+
+        default:
+            return state;
+    }
+}
